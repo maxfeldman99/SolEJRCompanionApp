f227add0d9e90aa81405a465630d7fb94bf38229
MAX FELDMAN <maxfeldman99@gmail.com>
2019-01-14 23:42:11 +0200

    initial classes were created

---
+++ app/src/main/java/com/example/maxfeldman/sole_jr_companionapp/Models/User.java
@@ -0,0 +1,32 @@
+package com.example.maxfeldman.sole_jr_companionapp.Models;
+
+/**
+ * Created by MAX FELDMAN on 14/01/2019.
+ */
+
+public class User {
+
+    private String name;
+    private String id;
+
+    public User(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
