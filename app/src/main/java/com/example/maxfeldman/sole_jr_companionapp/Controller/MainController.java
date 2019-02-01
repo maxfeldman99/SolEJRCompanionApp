d8b5b20fd3aaf0e92a4b6ac21860caa0085a7501
Michael Katan <michaelkatan@gmail.com>
2019-02-01 13:57:35 +0200

    Main Controller updated

--- app/src/main/java/com/example/maxfeldman/sole_jr_companionapp/Controller/MainController.java
+++ app/src/main/java/com/example/maxfeldman/sole_jr_companionapp/Controller/MainController.java
@@ -30,6 +30,8 @@ public class MainController {
     {
         Lesson animalLesson = new Lesson();
 
+
+
         animalLesson.setId(1);
         animalLesson.setBadge("jpn file");
         animalLesson.setCategory("English Animals Lesson");
@@ -38,6 +40,9 @@ public class MainController {
 
         String[] goals = {"Teach animals", "Teach english", "Teach colors"};
         animalLesson.setGoals(goals);
+
+
+        //Start of scenario1
 
         Scenario scenario1 = new Scenario();
         scenario1.setId(1);
@@ -49,43 +54,99 @@ public class MainController {
         action1.setTextOrWav("Can you say the name of this animals?");
         action1.setWhatToPlay("");
 
-        OnFailure failure = new OnFailure();
+        OnFailure failure1 = new OnFailure();
 
-        Action onFailureAction = new Action();
-        onFailureAction.setEffect("sadFace");
-        onFailureAction.setTextOrWav("Oh Oh, Try again!");
-        onFailureAction.setWhatToPlay("");
+        Action onFailureAction1 = new Action();
+        onFailureAction1.setEffect("sadFace");
+        onFailureAction1.setTextOrWav("Oh Oh, Try again!");
+        onFailureAction1.setWhatToPlay("");
 
-        failure.setAction(onFailureAction);
-        failure.setNumOfRetries(2);
-        failure.setNextScenarioID("2");
-
-
-        OnSuccess success = new OnSuccess();
-
-        Action onSuccessAction = new Action();
-        onSuccessAction = new Action();
-        onSuccessAction.setEffect("Smile");
-        onSuccessAction.setTextOrWav("Good job!!!");
-        onSuccessAction.setWhatToPlay("");
-
-        success.setAction(onSuccessAction);
-        success.setNextScenarioID("2");
-
-        scenario1.setOnSuccess(success);
-        scenario1.setOnfailure(failure);
-
-        WaitFor waitFor = new WaitFor();
-        ExpectedAnswer expectedAnswer = new ExpectedAnswer();
-        expectedAnswer.setInput("Dog");
-        expectedAnswer.setSuccessRating(80);
-
-        waitFor.setTypeOfWaiting(1);
-        waitFor.setExpectedAnswer(expectedAnswer);
+        failure1.setAction(onFailureAction1);
+        failure1.setNumOfRetries(2);
+        failure1.setNextScenarioID("2");
 
 
+        OnSuccess success1 = new OnSuccess();
+
+        Action onSuccessAction1 = new Action();
+        onSuccessAction1 = new Action();
+        onSuccessAction1.setEffect("Smile");
+        onSuccessAction1.setTextOrWav("Good job!!!");
+        onSuccessAction1.setWhatToPlay("");
+
+        success1.setAction(onSuccessAction1);
+        success1.setNextScenarioID("2");
+
+        scenario1.setOnSuccess(success1);
+        scenario1.setOnfailure(failure1);
+
+        WaitFor waitFor1 = new WaitFor();
+        ExpectedAnswer expectedAnswer1 = new ExpectedAnswer();
+        expectedAnswer1.setInput("Dog");
+        expectedAnswer1.setSuccessRating(80);
+
+        waitFor1.setTypeOfWaiting(1);
+        waitFor1.setExpectedAnswer(expectedAnswer1);
+
+        scenario1.setWaitFor(waitFor1);
+
+        //Start of scenario2
+        Scenario scenario2 = new Scenario();
+
+
+        scenario2.setId(2);
+        scenario2.setLevel("2");
+
+
+        Action action2 = new Action();
+        action2.setEffect("Flamingo.png");
+        action2.setTextOrWav("What color is the animal in the picture?");
+        action2.setWhatToPlay("");
+
+
+        OnFailure failure2 = new OnFailure();
+
+        Action onFailureAction2 = new Action();
+        onFailureAction2.setEffect("sadFace");
+        onFailureAction2.setTextOrWav("No worries, Try again!");
+        onFailureAction2.setWhatToPlay("");
+
+        failure2.setAction(onFailureAction2);
+        failure2.setNextScenarioID("2");
+        failure2.setNumOfRetries(2);
+
+        OnSuccess success2 = new OnSuccess();
+
+        Action onSuccessAction2 = new Action();
+        onSuccessAction2 = new Action();
+        onSuccessAction2.setEffect("Smile");
+        onSuccessAction2.setTextOrWav("Good job!!!");
+        onSuccessAction2.setWhatToPlay("");
+
+        success2.setAction(onSuccessAction2);
+        success2.setNextScenarioID("3");
+
+        WaitFor waitFor2= new WaitFor();
+        ExpectedAnswer expectedAnswer2 = new ExpectedAnswer();
+        expectedAnswer2.setInput("Pink");
+        expectedAnswer2.setSuccessRating(75);
+
+        waitFor2.setTypeOfWaiting(1);
+        waitFor2.setExpectedAnswer(expectedAnswer2);
+
+        scenario2.setWaitFor(waitFor2);
+        scenario2.setOnfailure(failure2);
+        scenario2.setOnSuccess(success2);
+
+
+
+
+        //getting things together
         Scenario[] scenarios = new Scenario[3];
-        scenarios[1] = scenario1;
+
+        scenarios[0] = scenario1;
+
+        scenarios[1] = scenario2;
 
         animalLesson.setScenarios(scenarios);
 
